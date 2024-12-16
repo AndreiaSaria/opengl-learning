3ec728875321f5e722998ac9c7d51080665e2d7b
Andréia Fernandez <andreia-f@outlook.com.br>
2024-12-16 13:01:27 -0300

    Added glad in project and rendered a triangle, separating shader classes now

---
+++ src/VertexShader.cpp
@@ -0,0 +1,21 @@
+#include "VertexShader.h"
+#include <glad/include/glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+unsigned int VertexShader::CreateVertexShader() const
+{
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    return vertexShader;
+}
